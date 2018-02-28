99aa11362336367796b21844ebe644b6f4facf74
name <email>
2018-02-28 14:32:28 +0900

    update 3way child program

--- 3way-gateway-child/3way-gateway-child.c
+++ 3way-gateway-child/3way-gateway-child.c
@@ -3,7 +3,7 @@
  */
 
 /*
- * can3way_child.c - can3-waytransfer.c used in combination
+ * can3way-gateway-child.c - can3-waytransfer.c used in combination
  *
  * Copyright (c) 2002-2007 Volkswagen Group Electronic Research
  * All rights reserved.
@@ -63,151 +63,144 @@
 
 #include "lib.c"
 
-int attackSend_CountorTime(int *s, int *nbyte, struct can_frame frame, int attack_count, double attack_cycle, double attack_time);
+#define CAN_RECV_SOCKET 0
+#define UDP_RECV_SOCKET 1
+
+int write_eth (int socket, struct sockaddr_in addr, struct can_frame *frame) {
+	char send_data[100];
+	sprint_canframe(send_data, frame, 0);
+	if (sendto(socket, send_data, strlen(send_data), 0, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("sendto_eth");
+		return -1;
+	}
+	printf("[dump CAN] %s\n", send_data);
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
-	/*argv[1]:can0, argv[2]:Data, argv[3]:Count, argv[4]:Cycle(us), argv[5]:Time(us)*/
-	int s_can; /* can raw socket */ 
+	fd_set rdfs;
+	int i;
+	int ret;
+	int s_udp_send; /* udp send socket */ 
+	int s[2]; /* s[0] is can socket, s[1] is udp recv socket*/
+	int currmax = 2;
 	int nbytes;
-	struct sockaddr_can addr;
+	struct sockaddr_can addr_can;
 	struct can_frame frame;
 	struct ifreq ifr;
-
-	/* check command line options */
-	/*
-	if (argc != 6) {
-		fprintf(stderr, "Usage: %s <device> <can_frame> <Data> <Count> <Cycle> <Time>.\n", argv[0]);
-		return 1;
-	}*/
-	
-	int s_udp;
-	struct sockaddr_in addr_udp;
+	struct sockaddr_in addr_udp_send;
+	struct sockaddr_in addr_udp_recv;
 	socklen_t sin_size = sizeof(struct sockaddr_in);
 	struct sockaddr_in from_addr;
+	char udp_recv_buf[1024];
+	struct iovec iov;
+	struct msghdr msg;
+	char ctrlmsg[CMSG_SPACE(sizeof(struct timeval)) + CMSG_SPACE(sizeof(__u32))];
 
-	char buf[1024];
-
-	if ((s_udp = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	if ((s_udp_send = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket");
+		return 1;
+	}
+	if ((s[1] = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket");
 		return 1;
 	}
 
-	addr_udp.sin_family = AF_INET;
-	addr_udp.sin_port = htons(4989);
-	addr_udp.sin_addr.s_addr = INADDR_ANY;
+	addr_udp_send.sin_family = AF_INET;
+	addr_udp_send.sin_port = htons(9894);
+	addr_udp_send.sin_addr.s_addr = inet_addr("169.254.115.249");//ip address 169.254.115.249 is 3way-transfer-parent
+	addr_udp_recv.sin_family = AF_INET;
+	addr_udp_recv.sin_port = htons(4989);
+	addr_udp_recv.sin_addr.s_addr = INADDR_ANY;
 
-	if (bind(s_udp, (struct sockaddr *)&addr_udp, sizeof(addr_udp)) < 0) {
+	if (bind(s[1], (struct sockaddr *)&addr_udp_recv, sizeof(addr_udp_recv)) < 0) {
+		perror("udp recv socket bind");
+		return 1;
+	}
+
+	memset(udp_recv_buf, 0, sizeof(udp_recv_buf));
+
+	/* open can socket */
+	if ((s[0] = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("socket");
+		return 1;
+	}
+
+	addr_can.can_family = AF_CAN;
+
+	strcpy(ifr.ifr_name, "can0");
+	if (ioctl(s[0], SIOCGIFINDEX, &ifr) < 0) {
+		perror("SIOCGIFINDEX");
+		return 1;
+	}
+	addr_can.can_ifindex = ifr.ifr_ifindex;
+
+	setsockopt(s[0], SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	if (bind(s[0], (struct sockaddr *)&addr_can, sizeof(addr_can)) < 0) {
 		perror("bind");
 		return 1;
 	}
 
-	memset(buf, 0, sizeof(buf));
-	/* open socket */
-	if ((s_can = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-		perror("socket");
-		return 1;
-	}
-
-	addr.can_family = AF_CAN;
-
-	strcpy(ifr.ifr_name, "can0");
-	if (ioctl(s_can, SIOCGIFINDEX, &ifr) < 0) {
-		perror("SIOCGIFINDEX");
-		return 1;
-	}
-	addr.can_ifindex = ifr.ifr_ifindex;
-
+	iov.iov_base = &frame;
+	msg.msg_name = &addr_can;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = &ctrlmsg;
+			
 	int running = 1;
 
-	printf("eth dump running\n");
+	printf("can and eth dump running\n");
 	while (running) {
-		printf("waiting eth packet\n");
-		if (recv(s_udp, buf, sizeof(buf), 0) < 0) {
-			perror("recv");
-			return 1;
-		}
-		printf("%s\n",buf);
-
-		/*Divide received data */
-		char *CANData, *attack_count, *attack_cycle, *attack_time, *cansendinfo[255];
-		int i = 0;
-		cansendinfo[0] = strtok(buf, ":");
-		do {
-			cansendinfo[++i] = strtok(NULL, ":");
-		} while ((i < 4) && (cansendinfo[i] != NULL));
-
-		CANData = cansendinfo[0];
-		attack_count = cansendinfo[1];
-		attack_cycle = cansendinfo[2];
-		attack_time = cansendinfo[3];
-		/* parse CAN frame */
-		if (parse_canframe(CANData, &frame)){
-			fprintf(stderr, "\nWrong CAN-frame format!\n\n");
-			fprintf(stderr, "Try: <can_id>#{R|data}\n");
-			fprintf(stderr, "can_id can have 3 (SFF) or 8 (EFF) hex chars\n");
-			fprintf(stderr, "data has 0 to 8 hex-values that can (optionally)");
-			fprintf(stderr, " be seperated by '.'\n\n");
-			fprintf(stderr, "e.g. 5A1#11.2233.44556677.88 / 123#DEADBEEF / ");
-			fprintf(stderr, "5AA# /\n     1F334455#1122334455667788 / 123#R ");
-			fprintf(stderr, "for remote transmission request.\n\n");
-			return 1;
-		}
-
-
-		/* disable default receive filter on this RAW socket */
-		/* This is obsolete as we do not read from the socket at all, but for */
-		/* this reason we can remove the receive list in the Kernel to save a */
-		/* little (really a very little!) CPU usage.                          */
-		setsockopt(s_can, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
-
-		if (bind(s_can, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-			perror("bind");
-			return 1;
-		}
+		FD_ZERO(&rdfs);
+		for (i = 0; i < currmax; i++)
+			FD_SET(s[i], &rdfs);
 		
-		attackSend_CountorTime(&s_can, &nbytes, frame, 1, 0 ,0);
+		printf("AAAAAAAAAAAAAAAAAAAAAAAA\n");
+		select(s[currmax-1]+1, &rdfs, NULL, NULL, NULL);
+		printf("BBBBBBBBBBBBBBBBBBBBBBBB\n");
+		for (i = 0; i < currmax; i++) {
+			if (FD_ISSET(s[i], &rdfs)) {
+
+				iov.iov_len = sizeof(frame);
+				msg.msg_namelen = sizeof(addr_can);
+				msg.msg_controllen = sizeof(ctrlmsg);
+				msg.msg_flags = 0;
+				
+				if (i == CAN_RECV_SOCKET) {
+					nbytes = recvmsg(s[i], &msg, 0);
+					if (nbytes < 0) {
+						perror("can read");
+						return 1;
+					}
+					if (nbytes < sizeof(struct can_frame)) {
+						fprintf(stderr, "can read: incomplete CAN frame\n");
+						return 1;
+					}
+					nbytes = write_eth(s_udp_send, addr_udp_send, &frame);
+				} else if (i == UDP_RECV_SOCKET) {
+					if (recv(s[i], udp_recv_buf, sizeof(udp_recv_buf), 0) < 0) {
+						perror("udp recv");
+						return 1;
+					}
+					/* From UDP packet to CAN frame*/
+					parse_canframe(udp_recv_buf, &frame);
+					/* disable default receive filter on this RAW socket */
+					/* This is obsolete as we do not read from the socket at all, but for */
+					/* this reason we can remove the receive list in the Kernel to save a */
+					/* little (really a very little!) CPU usage.                          */
+					//attackSend_CountorTime(&s_can, &nbytes, frame, 1, 0 ,0);
+					nbytes = write(s[0], &frame, sizeof(frame));
+					printf("[dump ETH] %s\n",udp_recv_buf);
+				}
+			}
+
+		}
 	}
 
-	close(s_udp);
-	close(s_can);
+	close(s_udp_send);
+	close(s[0]);
+	close(s[1]);
 	return 0;
-}
-
-int attackSend_CountorTime(int *s, int *nbytes, struct can_frame frame, int attack_count, double attack_cycle, double attack_time) {
-	/* When attack count is more than zero, Count attack execute */
-	/* otherwise,  Cycle attack execute */
-	if (attack_time/(1000*1000*1000) > 60) attack_time = 0;
-	struct timespec attack_Cycle;
-	attack_Cycle.tv_sec = 0;
-	attack_Cycle.tv_nsec = attack_cycle;
-	if (attack_count <= 0) {
-		/* Cycle attack*/
-		struct timespec start, end;
-		clock_gettime(CLOCK_REALTIME, &start);
-		end = start;
-		attack_time /= 1000;
-		double dur = (end.tv_sec - start.tv_sec)*(1000*1000) + (end.tv_nsec - start.tv_nsec)/1000;
-		while (dur < attack_time) {
-			/* send frame */
-			if ((*nbytes = write(*s, &frame, sizeof(frame))) != sizeof(frame)) {
-				perror("write");
-				return 1;
-			}
-			clock_nanosleep(CLOCK_REALTIME, 0, &attack_Cycle, NULL);
-			clock_gettime(CLOCK_REALTIME, &end);
-			dur = (end.tv_sec - start.tv_sec)*(1000*1000) + (end.tv_nsec - start.tv_nsec)/1000;
-		}
-	} else {
-		/*Count attack*/
-		int i;
-		for (i = 0; i < attack_count; i++) {
-			/* send frame */
-			if ((*nbytes = write(*s, &frame, sizeof(frame))) != sizeof(frame)) {
-				perror("write");
-				return 1;
-			}
-			clock_nanosleep(CLOCK_REALTIME, 0, &attack_Cycle, NULL);
-		}
-	}
 }
